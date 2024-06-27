4e8fec5905154e2d2429077b716955bef02c2bf0
jsmall <small.jordan@gmail.com>
2024-06-27 09:08:58 -0400

    chore: add global leader path and basic keymap

--- config/default.nix
+++ config/default.nix
@@ -1,4 +1,25 @@
 {
-  # Import all your configuration modules here
-  imports = [ ./bufferline.nix ];
+# Import all your configuration modules here
+  imports = [ 
+    ./extras/bufferline.nix
+  ];
+
+  colorschemes.catppuccin.enable = true;
+
+  globals.mapleader = " ";
+
+  keymaps = [
+  # File
+  {
+    mode = "n";
+    key = "<leader>f";
+    action = "+find/file";
+  }
+  {
+  # Format file
+    key = "<leader>fm";
+    action = "<CMD>lua vim.lsp.buf.format()<CR>";
+    options.desc = "Format the current buffer";
+  }
+  ];
 }
