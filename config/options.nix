d57b4772120ea5959e505ca909ae97f2c2eb46e9
jsmall <small.jordan@gmail.com>
2024-06-28 09:11:21 -0400

    feat(options): add basic default options for tabs etc

---
+++ config/options.nix
@@ -0,0 +1,22 @@
+{ lib, pkgs, ... }:
+{
+	config.opts = {
+	updatetime = 100;
+	fileencoding = "utf-8";
+
+	number = true;
+	relativenumber = true;
+
+	autoindent = true;
+	expandtab = true;
+	smartindent = true;
+	shiftwidth = 2;
+	tabstop = 2;
+
+	ignorecase = true;
+	smartcase = true;
+
+	swapfile = false;
+	undofile = true;
+	};
+}
