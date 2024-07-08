137e824355f018ae4c6368ecbf776b669b9c10b5
jsmall <small.jordan@gmail.com>
2024-07-08 12:56:01 -0400

    feat(nvim-tree): add pane switching key bindings

--- config/extras/nvim-tree.nix
+++ config/extras/nvim-tree.nix
@@ -4,4 +4,27 @@
 		openOnSetupFile = true;
 		autoReloadOnWrite = true;
 	};
+
+  keymaps = [
+  {
+      action = "<C-w>h";
+      key = "<C-h>";
+      mode = [ "n" ];
+    }
+  {
+      action = "<C-w>j";
+      key = "<C-j>";
+      mode = [ "n" ];
+    }
+  {
+      action = "<C-w>k";
+      key = "<C-k>";
+      mode = [ "n" ];
+    }
+  {
+      action = "<C-w>l";
+      key = "<C-l>";
+      mode = [ "n" ];
+    }
+  ];
 }
