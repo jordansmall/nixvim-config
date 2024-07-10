28d663d6d48971d1f77c9a1c6dad3ec9ada66c55
jsmall <small.jordan@gmail.com>
2024-07-10 10:51:50 -0400

    chore(copolit): ignore file types

--- config/extras/copilot.nix
+++ config/extras/copilot.nix
@@ -3,5 +3,14 @@
     enable = true;
     panel.enabled = false;
     suggestion.enabled = false;
+    filetypes = {
+        yaml = false;
+        markdown = false;
+        help = false;
+        gleam = false;
+        gitcommit = false;
+        gitrebase = false;
+        "." = false;
+      };
   };
 }
