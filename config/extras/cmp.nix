990f1821af7db18e6941064850e62c0a6181f9d1
jsmall <small.jordan@gmail.com>
2024-07-10 10:52:40 -0400

    fix(cmp): define sources rather than auto discovery

--- config/extras/cmp.nix
+++ config/extras/cmp.nix
@@ -10,7 +10,7 @@
 
     cmp = {
       enable = true;
-      autoEnableSources = true;
+      #autoEnableSources = true;
       settings = {
         snippet.expand = ''
           function(args)
@@ -20,6 +20,17 @@
 
         experimental = { ghost_text = true; };
 
+        sources = [
+          { name = "nvim_lsp"; }
+          { name = "luasnip"; }
+          {
+            name = "buffer";
+            option.get_bufnrs.__raw = "vim.api.nvim_list_bufs";
+          }
+          { name = "nvim_lua"; }
+          { name = "path"; }
+          { name = "copilot"; }
+        ];
         formatting = {
           fields = [ "abbr" "kind" "menu" ];
           format = 
@@ -102,16 +113,42 @@
           "<Tab>" =
             # lua 
             ''
-            function(fallback)
-            local line = vim.api.nvim_get_current_line()
-            if line:match("^%s*$") then
-              fallback()
+              function(fallback)
+                local line = vim.api.nvim_get_current_line()
+                if line:match("^%s*$") then
+                  fallback()
                 elseif cmp.visible() then
-                cmp.confirm({ behavior = cmp.ConfirmBehavior.Insert, select = true })
-            else
-              fallback()
+                  cmp.confirm({ behavior = cmp.ConfirmBehavior.Insert, select = true })
+                else
+                  fallback()
                 end
+              end
+            '';
+          "<Down>" =
+            # lua
+            ''
+              function(fallback)
+                if cmp.visible() then
+                  cmp.select_next_item()
+                elseif require("luasnip").expand_or_jumpable() then
+                  vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
+                else
+                  fallback()
                 end
+              end
+            '';
+          "<Up>" =
+            # lua
+            ''
+              function(fallback)
+                if cmp.visible() then
+                  cmp.select_prev_item()
+                elseif require("luasnip").jumpable(-1) then
+                  vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
+                else
+                  fallback()
+                end
+              end
             '';
         };
       };
