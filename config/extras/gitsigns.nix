07899ebf83b156f6ed44e00ff76ccf2ea5b96763
Jordan Small <jorsmall@jackhenry.com>
2024-07-11 16:43:23 -0400

    chore(gitsigns): add additional configuration

--- config/extras/gitsigns.nix
+++ config/extras/gitsigns.nix
@@ -4,6 +4,32 @@
     settings = {
       current_line_blame = true;
       trouble = true;
+      signcolumn = true;
+      numhl = false;
+      linehl = false;
+      word_diff = false;
+      watch_gitdir = { follow_files = true; };
+      auto_attach = true;
+      attach_to_untracked = true;
+      current_line_blame_opts = {
+        virt_text = true;
+        virt_text_pos = "eol";
+        delay = 1000;
+      };
+
+      current_line_blame_formatter =
+        "<author>, <author_time:%Y-%m-%d> - <summary>";
+      sign_priority = 6;
+      update_debounce = 100;
+      status_formatter = null; # Use default
+      max_file_length = 40000;
+      preview_config = {
+        border = "single";
+        style = "minimal";
+        relative = "cursor";
+        row = 0;
+        col = 1;
+      };
     };
   };
 
