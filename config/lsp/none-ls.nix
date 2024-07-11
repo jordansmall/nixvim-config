212c99f03b6442da700eb6a49b7cc53e60c578c3
Jordan Small <jorsmall@jackhenry.com>
2024-07-11 16:41:37 -0400

    feat(none-ls): add additional diagnostics and formatting servers

--- config/lsp/none-ls.nix
+++ config/lsp/none-ls.nix
@@ -2,11 +2,19 @@
   plugins.none-ls = {
     enable = true;
     sources = {
+      diagnostics = {
+        golangci_lint.enable = true;
+        ktlint.enable = true;
+        statix.enable = true;
+      };
       formatting = {
         gofmt.enable = true;
         goimports.enable = true;
         ktlint.enable = true;
         nixfmt.enable = true;
+        shfmt.enable = true;
+        markdownlint.enable = true;
+        shellharden.enable = true;
       };
     };
   };
