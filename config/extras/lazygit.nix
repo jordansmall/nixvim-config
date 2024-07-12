a0f2e1e3ca0d3b2726f436974abcb4ab3b86b056
Jordan Small <jorsmall@jackhenry.com>
2024-07-12 11:28:33 -0400

    feat(lazygit): add initial configuration

---
+++ config/extras/lazygit.nix
@@ -0,0 +1,15 @@
+{
+  plugins.lazygit = {
+    enable = true;
+    settings = {
+      config_file_path = [ ];
+      floating_window_border_chars = [ "╭" "─" "╮" "│" "╯" "─" "╰" "│" ];
+      floating_window_scaling_factor = 0.9;
+      floating_window_use_plenary = false;
+      floating_window_winblend = 0;
+      use_custom_config_file_path = false;
+      use_neovim_remote = true;
+    };
+
+  };
+}
