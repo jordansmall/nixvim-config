d6830ff486732aeb80f16cfda6b08523f9e89090
Jordan Small <jorsmall@jackhenry.com>
2024-07-23 10:20:38 -0400

    chore: add basic telescope keybindings

--- config/utils/telescope.nix
+++ config/utils/telescope.nix
@@ -2,7 +2,10 @@
   plugins.telescope = {
     enable = true;
     keymaps = {
+      "<leader>ff" = "find_files";
       "<leader>fg" = "live_grep";
+      "<leader>gs" = "git_status";
+      "<leader>cd" = "lsp_definitions";
       "<C-p>" = {
         action = "git_files";
         options = { desc = "Telescope Git Files"; };
