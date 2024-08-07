76dd1fe7ac48e5b97560b0ebbf61d26c24009b30
Jordan Small <jorsmall@jackhenry.com>
2024-08-07 09:54:31 -0400

    chore: add additional lsps and keymaps

--- config/lsp/default.nix
+++ config/lsp/default.nix
@@ -2,24 +2,106 @@
   plugins = {
     lsp = {
       enable = true;
+      inlayHints = true;
       servers = {
         bashls.enable = true;
         gopls.enable = true;
         kotlin-language-server.enable = true;
-        nixd.enable = true;
-        #ansiblels.enable = true;
+        nil-ls.enable = true;
+        jsonls.enable = true;
+        ansiblels.enable = true;
+        helm-ls = {
+          enable = true;
+          extraOptions = {
+            settings = {
+              "helm-ls" = {
+                yamlls = {
+                  path =
+                    "${pkgs.yaml-language-server}/bin/yaml-language-server";
+                };
+              };
+            };
+          };
+        };
+        yamlls = {
+          enable = true;
+          extraOptions = {
+            settings = {
+              yaml = {
+                schemas = {
+                  kubernetes = "'*.yaml";
+                  "http://json.schemastore.org/github-workflow" =
+                    ".github/workflows/*";
+                  "http://json.schemastore.org/github-action" =
+                    ".github/action.{yml,yaml}";
+                  "http://json.schemastore.org/ansible-stable-2.9" =
+                    "roles/tasks/*.{yml,yaml}";
+                  "http://json.schemastore.org/kustomization" =
+                    "kustomization.{yml,yaml}";
+                  "http://json.schemastore.org/ansible-playbook" =
+                    "*play*.{yml,yaml}";
+                  "http://json.schemastore.org/chart" = "Chart.{yml,yaml}";
+                  "https://json.schemastore.org/dependabot-v2" =
+                    ".github/dependabot.{yml,yaml}";
+                  "https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json" =
+                    "*docker-compose*.{yml,yaml}";
+                  "https://raw.githubusercontent.com/argoproj/argo-workflows/master/api/jsonschema/schema.json" =
+                    "*flow*.{yml,yaml}";
+                };
+              };
+            };
+          };
+        };
       };
       keymaps = {
-        diagnostics = {
-          "<leader>k" = "goto_prev";
-          "<leader>j" = "goto_next";
-        };
+        silent = true;
         lspBuf = {
-          "gd" = "definition";
-          "gr" = "<cmd>Telescope lsp_references<CR>";
-          "gt" = "type_definition";
-          "gi" = "implementation";
-          "K" = "hover";
+          gd = {
+            action = "definition";
+            desc = "Goto Definition";
+          };
+          gr = {
+            action = "references";
+            desc = "Goto References";
+          };
+          gD = {
+            action = "declaration";
+            desc = "Goto Declaration";
+          };
+          gI = {
+            action = "implementation";
+            desc = "Goto Implementation";
+          };
+          gT = {
+            action = "type_definition";
+            desc = "Type Definition";
+          };
+          K = {
+            action = "hover";
+            desc = "Hover";
+          };
+          "<leader>cw" = {
+            action = "workspace_symbol";
+            desc = "Workspace Symbol";
+          };
+          "<leader>cr" = {
+            action = "rename";
+            desc = "Rename";
+          };
+        };
+        diagnostic = {
+          "<leader>cd" = {
+            action = "open_float";
+            desc = "Line Diagnostics";
+          };
+          "<leader>j" = {
+            action = "goto_next";
+            desc = "Next Diagnostic";
+          };
+          "<leader>k" = {
+            action = "goto_prev";
+            desc = "Previous Diagnostic";
+          };
         };
       };
     };
