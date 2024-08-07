f95405f150a9c27a3f179c45f8271a8c4b807286
Jordan Small <jorsmall@jackhenry.com>
2024-08-07 09:52:50 -0400

    chore: move telescope keybindings into treessitter with additional

--- config/utils/telescope.nix
+++ config/utils/telescope.nix
@@ -1,16 +1,16 @@
 {
   plugins.telescope = {
     enable = true;
-    keymaps = {
-      "<leader>ff" = "find_files";
-      "<leader>fg" = "live_grep";
-      "<leader>gs" = "git_status";
-      "<leader>cd" = "lsp_definitions";
-      "<C-p>" = {
-        action = "git_files";
-        options = { desc = "Telescope Git Files"; };
-      };
-    };
+#    keymaps = {
+#      "<leader>ff" = "find_files";
+#      "<leader>fg" = "live_grep";
+#      "<leader>gs" = "git_status";
+#      "<leader>cd" = "lsp_definitions";
+#      "<C-p>" = {
+#        action = "git_files";
+#        options = { desc = "Telescope Git Files"; };
+#      };
+#    };
     extensions.fzf-native = { enable = true; };
   };
 }
