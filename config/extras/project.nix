ee11cea8455f25adbf330a0846c68c431d725e2b
Jordan Small <jorsmall@jackhenry.com>
2026-06-17 13:19:31 -0400

    fix(project): enable silent_chdir to suppress chdir notifications

--- config/extras/project.nix
+++ config/extras/project.nix
@@ -5,8 +5,8 @@
     settings = {
       # Anchor patterns used to detect project roots. Checked in order.
       patterns = [ ".git" ".hg" "Makefile" "package.json" "flake.nix" ];
-      # Do not auto-chdir on BufEnter; workspace switcher manages cwd explicitly.
-      silent_chdir = false;
+      # Suppress the chdir notification; workspace switcher manages cwd explicitly.
+      silent_chdir = true;
     };
   };
 }
