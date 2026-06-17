28f4bbdd85a64cd68aecefddd1eb9b73537dd0f8
Jordan Small <jorsmall@jackhenry.com>
2026-06-17 11:06:30 -0400

    fix(project): set silent_chdir = false to stop auto cwd changes
    Workspace switcher manages cwd explicitly via tcd; automatic global
    cwd changes on BufEnter would race with that.
    
    Closes #39

--- config/extras/project.nix
+++ config/extras/project.nix
@@ -5,8 +5,8 @@
     settings = {
       # Anchor patterns used to detect project roots. Checked in order.
       patterns = [ ".git" ".hg" "Makefile" "package.json" "flake.nix" ];
-      # Silently change cwd to project root on BufEnter; no echo.
-      silent_chdir = true;
+      # Do not auto-chdir on BufEnter; workspace switcher manages cwd explicitly.
+      silent_chdir = false;
     };
   };
 }
