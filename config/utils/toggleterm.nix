ea8df4dc7b0478e8e454dcb4209e7c51ff1f5659
Jordan Small <jorsmall@jackhenry.com>
2026-06-18 09:32:10 -0400

    feat(toggleterm): size floating window to 90% of editor dimensions
    - add float_opts.width and float_opts.height as Lua expressions computing
      90% of vim.o.columns / vim.o.lines at open time so the window tracks
      editor size correctly

--- config/utils/toggleterm.nix
+++ config/utils/toggleterm.nix
@@ -5,6 +5,10 @@
       direction = "float";
       insert_mappings = true;
       open_mapping = "[[<c-return>]]";
+      float_opts = {
+        width.__raw = "math.floor(vim.o.columns * 0.9)";
+        height.__raw = "math.floor(vim.o.lines * 0.9)";
+      };
     };
   };
 }
